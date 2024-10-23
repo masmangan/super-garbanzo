e1aa4f8b2b5ad24a2dea6fe62ff0ffdbb1fdba7e
Marco <marco.mangan@gmail.com>
2024-10-23 15:23:57 +0000

    This change adds a base configuration for Maven and JUnit.

---
+++ pizzas/src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pizzas/src/test/java/com/example/AppTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        int expected = 2;
+        int actual = App.sum(1, 1);
+
+        assertTrue( expected == actual);
+    }
+}
